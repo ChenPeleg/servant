5425a8ee7cf17433d071479aaa2c767012644601
Chen Peleg <chenpeleg@gmail.com>
2024-05-08 12:26:25 +0300

    api calls

--- public/assets/main.js
+++ public/assets/main.js
@@ -4,6 +4,10 @@ class ApiCall {
         this.query = query || {};
         this.body = body || {};
         this.method = method || 'GET';
+        this.name = name || 'Unknown';
+        this.id = `${name}${path}${JSON.stringify(query)}${method}`
+            .split('')
+            .reduce((s, c) => (Math.imul(31, s) + c.charCodeAt(0)) | 0, 0);
     }
 }
 
@@ -12,7 +16,21 @@ class Main {
         const button = document.querySelector('#callButton');
         console.log('init', button);
         this.select = document.querySelector('#callSelect');
+        this.calls().forEach((call) => {
+            const option = document.createElement('option');
+            option.text = call.name;
+            option.value = call.id;
+            this.select.add(option);
+        });
+
+        this.select.addEventListener('change', (ev) => {
+            this.selectionChanged(ev);
+        });
         button.onclick = (ev) => this.callApi(ev);
+    }
+
+    selectionChanged(ev) {
+        console.log('selectionChanged', this.select.value);
     }
 
     callApi(ev) {
