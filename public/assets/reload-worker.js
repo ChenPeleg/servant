191c30999e63ab0270664a73e0df23376f0f0184
Chen Peleg <chenpeleg@gmail.com>
2024-05-10 01:31:57 +0300

    added the controller in a different file

--- public/assets/reload-worker.js
+++
@@ -1,8 +0,0 @@
-onconnect = (e) => {
-    const port = e.ports[0];
-    const evtSource = new EventSource('http://localhost:8000');
-    evtSource.addEventListener('message', (e) => {
-        port.postMessage(e.data);
-    });
-    port.start();
-};
